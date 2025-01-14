677e9eb192caa737609f94c92cae35418babccec
Sakif-0713 <moktadirskf1@gmail.com>
2025-01-14 12:19:05 +0600

    ShinyButton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,52 @@
+import { Heading } from "./components/heading"
+import { MaxWidthWrapper } from "./components/MaxWidthWrapper"
+import { Check } from "lucide-react"
+import { ShinyButton } from "./components/ShinyButton"
 const Page = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <section className="relative py-24 sm:py-32 bg-brand-25">
+        <MaxWidthWrapper className="text-center">
+          <div className="relative mx-auto text-center flex flex-col items-center gap-10">
+            <div>
+              <Heading>
+                <span>Real-Time SaaS Insights,</span>
+                <br />{" "}
+                <span className="relative bg-gradient-to-r from-brand-700 to-brand-800 text-transparent bg-clip-text">
+                  Delivered to Your Discord
+                </span>
+              </Heading>
+            </div>
+            <p className="text-base/7 text-gray-600 max-w-prose text-center text-pretty">
+              PingPanda is the easiest way to monitor your Saas. Get instant
+              notifications{" "}
+              <span className="font-bold text-gray-700">
+                sales, new users, or any other event{" "}
+              </span>
+              sent directly to your Discord.
+            </p>
+            <ul className="space-y-2 text-base/7 text-gray-600 text-left flex flex-col items-start">
+              {[
+                "Real-Time Discord alerts for critical events",
+                "Buy once, use forever",
+                "Track sales, new users, or any other event,",
+              ].map((item, index) => (
+                <li key={index} className="flex items-center gap-1.5 text-left">
+                  <Check className="size-5 shrink-0 text-brand-700" />
+                  {item}
+                </li>
+              ))}
+            </ul>
+            <div className="w-full max-w-80">
+              <ShinyButton href="/sign-up" className="relative z-10 h-14 w-full text-base shadow-lg transition-shadow duration-300 hover:shadow-xl">Start For Free Today</ShinyButton>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </>
   )
 }
 export default Page
